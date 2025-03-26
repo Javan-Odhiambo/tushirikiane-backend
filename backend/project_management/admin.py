18f1e6c74fc08cccd25ac160669530582d6b46d8
Javan Otieno <javan.o.otieno@gmail.com>
2025-03-26 21:33:37 +0300

    Rename Database fields and register django models.
    Signed-off-by: Javan Otieno <javan.o.otieno@gmail.com>

--- backend/project_management/admin.py
+++ backend/project_management/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from project_management.models import Board, CheckListItem, Invite, Label, Task, TaskAssignee, TaskLabel, TaskList, \
+	Workspace, WorkspaceMember
+
 
 # Register your models here.
+
+@admin.register(Workspace)
+class WorkspaceAdmin(admin.ModelAdmin):
+	list_display = ["id", "name", "owner", "created_at"]
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+	list_display = ["id", "name", "workspace", "position"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+	list_display = ["id", "name", "task_list", "is_completed"]
+
+
+@admin.register(TaskList)
+class TaskListAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Invite)
+class InviteAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(TaskAssignee)
+class TaskAssigneeAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(CheckListItem)
+class CheckListItemAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(TaskLabel)
+class TaskLabelAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(WorkspaceMember)
+class WorkspaceMemberAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Label)
+class LabelAdmin(admin.ModelAdmin):
+	pass
--- backend/project_management/models.py
+++ backend/project_management/models.py
@@ -1,18 +1,20 @@
 import uuid
-from django.db import models
 
 from core.models import BaseModel
+from django.db import models
+
+app_label = "project_management"
 
 class Workspace(BaseModel):
     """
     Workspace model to represent a workspace in the project management system.
     Attributes:
         name (CharField): The name of the workspace.
-        owner_id (ForeignKey): The user who created the workspace.
+        owner (ForeignKey): The user who created the workspace.
     """
     
     name = models.CharField(max_length=255)
-    owner_id = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name="workspaces")
+    owner = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name="workspaces")
 
     def __str__(self):
         """
@@ -28,12 +30,12 @@ class WorkspaceMember(BaseModel):
     """
     WorkspaceMember model to represent a member in a workspace.
     Attributes:
-        workspace_id (ForeignKey): The workspace to which the member belongs.
-        member_id (ForeignKey): The user who is a member of the workspace.
+        workspace (ForeignKey): The workspace to which the member belongs.
+        member (ForeignKey): The user who is a member of the workspace.
     """
-    
-    workspace_id = models.ForeignKey(Workspace, on_delete=models.CASCADE, related_name="members")
-    member_id = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name="workspace_members")
+
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE, related_name="members")
+    member = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name="workspace_members")
 
 
     def __str__(self):
@@ -52,12 +54,12 @@ class Board(BaseModel):
     Attributes:
         name (CharField): The name of the board.
         description (TextField): A description of the board.
-        workspace_id (ForeignKey): The workspace to which the board belongs.
+        workspace (ForeignKey): The workspace to which the board belongs.
     """
     
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    workspace_id = models.ForeignKey(Workspace, on_delete=models.CASCADE, related_name="boards")
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE, related_name="boards")
     position = models.PositiveIntegerField()
     
     def __str__(self):
@@ -74,13 +76,13 @@ class TaskList(BaseModel):
     TaskList model to represent a list of tasks in a board.
     Attributes:
         name (CharField): The name of the list.
-        board_id (ForeignKey): The board to which the list belongs.
+        board (ForeignKey): The board to which the list belongs.
         position (PositiveIntegerField): The position of the list in the board.
         description (TextField): A description of the list.
     """
     
     name = models.CharField(max_length=255)
-    board_id = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="task_lists")
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="task_lists")
     position = models.PositiveIntegerField()
     description = models.TextField(blank=True)
 
@@ -100,7 +102,7 @@ class Task(BaseModel):
     Attributes:
         name (CharField): The name of the task.
         description (TextField): A description of the task.
-        task_list_id (ForeignKey): The list to which the task belongs.
+        task_list (ForeignKey): The list to which the task belongs.
         position (PositiveIntegerField): The position of the task in the list.
         due_date (DateField): The due date for the task.
         is_completed (BooleanField): Indicates whether the task is completed.
@@ -108,7 +110,7 @@ class Task(BaseModel):
     
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    task_list_id = models.ForeignKey(TaskList, on_delete=models.CASCADE, related_name="tasks")
+    task_list = models.ForeignKey(TaskList, on_delete=models.CASCADE, related_name="tasks")
     position = models.PositiveIntegerField()
     due_date = models.DateField(null=True, blank=True)
     is_completed = models.BooleanField(default=False)
@@ -127,12 +129,12 @@ class TaskAssignee(BaseModel):
     """
     TaskAssignee model to represent the assignment of a user to a task.
     Attributes:
-        task_id (ForeignKey): The task to which the assignee is assigned.
-        assignee_id (ForeignKey): The user who is assigned to the task.
+        task (ForeignKey): The task to which the assignee is assigned.
+        assignee (ForeignKey): The user who is assigned to the task.
     """
-    
-    task_id = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="assignees")
-    assignee_id = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name="assigned_tasks")
+
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="assignees")
+    assignee = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name="assigned_tasks")
 
     def __str__(self):
         """
@@ -150,16 +152,16 @@ class CheckListItem(BaseModel):
     Attributes:
         name (CharField): The name of the checklist item.
         is_completed (BooleanField): Indicates whether the checklist item is completed.
-        task_id (ForeignKey): The task to which the checklist item belongs.
+        task (ForeignKey): The task to which the checklist item belongs.
         due_at (DateTimeField): The due date and time for the checklist item.
-        assignee_id (ForeignKey): The user who is assigned to complete the checklist item.
+        assignee (ForeignKey): The user who is assigned to complete the checklist item.
     """
     
     name = models.CharField(max_length=255)
     is_completed = models.BooleanField(default=False)
-    task_id = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="checklist_items")
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="checklist_items")
     due_at = models.DateTimeField(null=True, blank=True)
-    assignee_id = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name="checklist_items")
+    assignee = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name="checklist_items")
 
     def __str__(self):
         """
@@ -195,12 +197,12 @@ class TaskLabel(BaseModel):
     """
     TaskLabel model to represent the assignment of a label to a task.
     Attributes:
-        task_id (ForeignKey): The task to which the label is assigned.
-        label_id (ForeignKey): The label assigned to the task.
+        task (ForeignKey): The task to which the label is assigned.
+        label (ForeignKey): The label assigned to the task.
     """
-    
-    task_id = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="labels")
-    label_id = models.ForeignKey(Label, on_delete=models.CASCADE, related_name="tasks")
+
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="labels")
+    label = models.ForeignKey(Label, on_delete=models.CASCADE, related_name="tasks")
 
     def __str__(self):
         """
@@ -216,15 +218,15 @@ class Invite(BaseModel):
     """
     Invite model to represent an invitation to join a workspace.
     Attributes:
-        workspace_id (ForeignKey): The workspace for which the invite is sent.
-        sender_id (ForeignKey): The user who sent the invite.
+        workspace (ForeignKey): The workspace for which the invite is sent.
+        sender (ForeignKey): The user who sent the invite.
         recipient_email (EmailField): The email address of the recipient.
         token (UUIDField): The token used for accepting the invite.
         is_accepted (BooleanField): Indicates whether the invite has been accepted.
     """
-    
-    workspace_id = models.ForeignKey(Workspace, on_delete=models.CASCADE, related_name="invites")
-    sender_id = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name="sent_invites")
+
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE, related_name="invites")
+    sender = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name="sent_invites")
     recipient_email = models.EmailField()
 
     # Internal fields
