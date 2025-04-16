9a129513a6a4a7eb9140ff6f251c5d84bf08d486
Karume-lab <danielkarume@outlook.com>
2025-04-16 23:19:22 +0300

    fix: :adhesive_bandage: minor fixes

--- frontend/src/app/(pages)/(protected)/workspaces/[workSpacesSlug]/page.tsx
+++ frontend/src/app/(pages)/(protected)/workspaces/[workSpacesSlug]/page.tsx
@@ -1,10 +1,24 @@
+import { Paper, Stack, Text, Title } from "@mantine/core";
 
-const SlugPage = () => {
+const WorkSpaceSlugPage = () => {
   return (
-    <div>
-      workspace slugname
-    </div>
+    <Paper
+      shadow="md"
+      p="xl"
+      radius="md"
+      maw={500}
+      mx={{ base: 16, sm: "auto" }}
+      mt={"25vh"}
+      withBorder
+    >
+      <Stack align="center" gap="sm">
+        <Title order={2}>No Board Selected</Title>
+        <Text size="md" ta="center" c="dimmed">
+          Please select a board to get started.
+        </Text>
+      </Stack>
+    </Paper>
   );
 };
 
-export default SlugPage;
+export default WorkSpaceSlugPage;
