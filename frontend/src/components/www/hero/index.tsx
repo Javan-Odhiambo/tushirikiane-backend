127506932d0d6cb533e22677229275e8aa4d78fb
Karume-lab <danielkarume@outlook.com>
2025-09-14 13:55:27 +0300

    fix: :lipstick: make hero section text more visible

--- frontend/src/components/www/hero/index.tsx
+++ frontend/src/components/www/hero/index.tsx
@@ -1,9 +1,9 @@
 "use client";
-import BlackButton from "@/components/core/Button";
-import { URLS } from "@/lib/urls";
 import { rem, Text } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import Image from "next/image";
+import BlackButton from "@/components/core/Button";
+import { URLS } from "@/lib/urls";
 
 export const BoldWord = ({ word }: { word: string }) => {
   const isMobile = useMediaQuery("(max-width: 720px)");
@@ -48,7 +48,7 @@ const Hero = () => {
         alt={"Hero section grid"}
         // objectFit={"cover"}
         layout={"fill"}
-        className={"-z-2"}
+        className={"-z-2 opacity-70"}
       />
       <section className="container h-[calc(100vh-120px)] flex flex-col justify-center items-center text-center">
         <h1 className="font-bold md:font-medium tracking-tight leading-[50px] mb-2 md:mb-4  text-4xl text-center md:text-8xl md:leading-[120px]">
@@ -58,7 +58,7 @@ const Hero = () => {
           my={rem(isMobile ? 36 : 28)}
           size={rem(isMobile ? 16 : 20)}
           lh={isMobile ? 1.2 : 1.5}
-          className="max-w-[900px] text-center"
+          className="max-w-[900px] text-center z-50"
         >
           We help teams like yours stay organized, track projects, and get
           things done—without breaking the bank. No monthly fees, no
